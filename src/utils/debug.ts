deb66e02a3a28af67a62e950ba4129d5dd0bbbe7
sato1043 <sato1043@updater.cc>
2024-08-11 18:11:46 +0900

    build: chore

--- src/utils/debug.ts
+++ src/utils/debug.ts
@@ -1,3 +1,3 @@
-import packageJson from '../../package.json';
 import debug from "debug";
-export default debug(`${packageJson.name}`);
+import { PACKAGE_NAME } from "../consts";
+export default debug(`${PACKAGE_NAME}`);
